faca25638bbae9d4fceca694ad36a0502bfd7d1b
李闯 <lichuang.lc@elecwatt.com>
2022-01-31 13:55:14 +0800

    合并两个有序链表

--- src/com/leeup/algorithm/leetcode/MergeTwoSortedLists.java
+++ src/com/leeup/algorithm/leetcode/MergeTwoSortedLists.java
@@ -16,21 +16,21 @@ public class MergeTwoSortedLists {
         ListNode cur = res;           //用来移动的cur
 
 
-        while (l1!=null && l2!=null){
+        while (l1 != null && l2 != null) {
             //l1<l2
-            if (l1.val <= l2.val){
+            if (l1.val <= l2.val) {
                 cur.next = l1;
                 l1 = l1.next;
-            }else {
+            } else {
                 cur.next = l2;
                 l2 = l2.next;
             }
             cur = cur.next;
         }
-        if (l1==null){
+        if (l1 == null) {
             cur.next = l2;
         }
-        if (l2==null){
+        if (l2 == null) {
             cur.next = l1;
         }
         return res.next;
@@ -51,6 +51,39 @@ public class MergeTwoSortedLists {
             this.val = val;
             this.next = next;
         }
+    }
+
+    public ListNode mergeListNodes(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1), p = dummy;
+        ListNode p1 = l1, p2 = l2;
+
+        while (l1 != null && l2 != null) {
+            if (p1.val > p2.val) {
+                p.next = p2;
+
+                // 将p2后移一位
+                p2 = p2.next;
+            } else {
+                p.next = p1;
+
+                //同理每次位置接到p后，将p1后移用于下次比较
+                p1 = p1.next;
+            }
+
+            //最后将p也后移，其实这里会为null，但是下次循环的时候就会赋值
+            p = p.next;
+
+        }
+
+        // 上面循环结束后，如果有任何一个还不为空，直接放进去
+        if (p1 != null) {
+            p.next = p1;
+        }
+
+        if (p2 != null) {
+            p.next = p2;
+        }
+        return dummy.next;
     }
 
 }
